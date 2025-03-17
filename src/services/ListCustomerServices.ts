a68cf81f363be43d0baf04c9a929e2c1b5d32336
Élcio Reis <elcio.reis024@gmail.com>
2025-03-17 19:47:10 -0300

    teste cancel

--- src/services/ListCustomerServices.ts
+++ src/services/ListCustomerServices.ts
@@ -3,7 +3,6 @@ import prismaClient from "../prisma";
 class ListCustomerServices {
   async execute() {
     const customersList = await prismaClient.customer.findMany();
-
     return customersList;
   }
 }
